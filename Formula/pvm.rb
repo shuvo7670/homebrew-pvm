bfb2ea73d1fe0a6122956051e11c3981f0c0aa8f
MD Shuvo <66669067+shuvo7670@users.noreply.github.com>
2025-04-17 12:34:29 +0600

    rename version

--- Formula/pvm.rb
+++ Formula/pvm.rb
@@ -1,7 +1,7 @@
 class Pvm < Formula
     desc "PHP-based CLI version manager"
     homepage "https://github.com/shuvo7670/homebrew-pvm"
-    url "https://github.com/shuvo7670/homebrew-pvm/releases/download/v1.0.0/pvm"
+    url "https://github.com/shuvo7670/homebrew-pvm/releases/download/1.0.0/pvm"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
     depends_on "php"
